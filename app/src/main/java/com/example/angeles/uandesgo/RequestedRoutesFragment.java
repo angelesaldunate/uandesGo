dad8aa2f910bcf228827e80c1e5330617ada0397
Alfonso <ajzuniga1@miuandes.cl>
2018-07-02 19:38:21 -0400

    Implemented swipe listview

--- app/src/main/java/com/example/angeles/uandesgo/RequestedRoutesFragment.java
+++ app/src/main/java/com/example/angeles/uandesgo/RequestedRoutesFragment.java
@@ -3,6 +3,8 @@ package com.example.angeles.uandesgo;
 import android.arch.persistence.room.Room;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
@@ -15,6 +17,10 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.baoyz.swipemenulistview.SwipeMenu;
+import com.baoyz.swipemenulistview.SwipeMenuCreator;
+import com.baoyz.swipemenulistview.SwipeMenuItem;
+import com.baoyz.swipemenulistview.SwipeMenuListView;
 import com.example.angeles.uandesgo.db.AppDatabase;
 import com.example.angeles.uandesgo.db.RequestedRoute.RequestedRoute;
 import com.example.angeles.uandesgo.db.Route.Route;
@@ -52,7 +58,7 @@ public class  RequestedRoutesFragment extends Fragment {
     }
 
     public void onViewCreated(View view, Bundle savedInstanceState){
-        final ListView lv = (ListView) view.findViewById(R.id.list_requested);
+        final SwipeMenuListView listView = view.findViewById(R.id.list_requested);
         final String value1 = sharedPreferences.getString("email_guardado",null);
         new Thread(new Runnable() {
             @Override
@@ -69,7 +75,42 @@ public class  RequestedRoutesFragment extends Fragment {
                     @Override
                     public void run() {
                         final RoutesAdapter adapter = new RoutesAdapter(getContext(), allroutes);
-                        lv.setAdapter(adapter);
+                        listView.setAdapter(adapter);
+                        SwipeMenuCreator creator = new SwipeMenuCreator() {
+
+                            @Override
+                            public void create(SwipeMenu menu) {
+                                // create "open" item
+                                SwipeMenuItem openItem = new SwipeMenuItem(getContext());
+                                // set item background
+                                openItem.setBackground(new ColorDrawable(Color.RED));
+                                // set item width
+                                openItem.setWidth(170);
+                                // set item title
+                                openItem.setIcon(R.drawable.ic_delete_white);
+                                // set item title fontsize
+                                openItem.setTitleSize(24);
+                                // set item title font color
+                                openItem.setTitleColor(Color.RED);
+                                // add to menu
+                                menu.addMenuItem(openItem);
+                            }
+                        };
+
+// set creator
+                        listView.setMenuCreator(creator);
+                        listView.setOnMenuItemClickListener(new SwipeMenuListView.OnMenuItemClickListener() {
+                            @Override
+                            public boolean onMenuItemClick(int position, SwipeMenu menu, int index) {
+                                switch (index) {
+                                    case 0:
+                                        Toast.makeText(getContext(), String.valueOf(allroutes.get(position).getPlaceId()), Toast.LENGTH_SHORT).show();
+                                        break;
+                                }
+                                // false : close the menu; true : not close the menu
+                                return false;
+                            }
+                        });
 
                     }
                 });
