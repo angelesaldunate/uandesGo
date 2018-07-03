8f968082025bf9854749d563daba77448f241a7d
unknown <ajzuniga1@miuandes.cl>
2018-07-03 16:38:11 -0400

    Added splash screen

---
+++ app/src/main/java/com/example/angeles/uandesgo/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.example.angeles.uandesgo;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private final int DURATION_SPLASH = 2250;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, DURATION_SPLASH);
+    }
+}
